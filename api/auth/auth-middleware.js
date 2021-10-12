5b6c326f12b25f961bcef64384db541dd4648825
dhef415 <dhef415@gmail.com>
2021-10-12 16:53:04 -0700

    completed project

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -28,31 +28,33 @@ const checkUsernameFree = async (req, res, next) => {
 }
 
 const checkUsernameExists = async (req, res, next) => {
-  try{
+  try {
     const users = await User.findBy({ username: req.body.username })
-    if (users.length) { 
+    if (users.length) {
       req.user = users[0]
       next()
-    }
-    else {
-      next({ message: 'Invalid credentials', status: 401 })
+    } else {
+      next({
+        status: 401,
+        message: 'Invalid credentials',
+      })
     }
   } catch (err) {
     next(err)
   }
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
+const checkPasswordLength = (req, res, next) => {
+  if (!req.body.password || req.body.password.length < 3) {
+    next({
+      status: 422,
+      message: 'Password must be longer than 3 chars',
+    })
+  } else {
+    next()
   }
-*/
-function checkPasswordLength() {}
+}
 
-// Don't forget to add these to the `exports` object so they can be required in other modules
 module.exports = {
   restricted,
   checkPasswordLength,
--- api/server.js
+++ api/server.js
@@ -41,8 +41,8 @@ server.get('/', (req, res) => {
   res.json({ api: 'up' })
 })
 
-server.use((err, req, res, next) => {
-  // eslint-disable-line
+// eslint-disable-next-line
+server.use((err, req, res, next) => { 
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
